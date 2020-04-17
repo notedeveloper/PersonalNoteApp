77dd83841715c948fdb2b17d70761f205078e206
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-17 15:36:54 +0800

    规范化备忘录Fragment的变量命名

--- app/src/main/java/com/exmply/personalnote/memorandum/MemorandumFragment.java
+++ app/src/main/java/com/exmply/personalnote/memorandum/MemorandumFragment.java
@@ -4,16 +4,17 @@ package com.exmply.personalnote.memorandum;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AlertDialog;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 
 
 import android.app.AlarmManager;
@@ -33,32 +34,33 @@ import com.exmply.personalnote.R;
 import java.util.Calendar;
 
 public class MemorandumFragment extends Fragment {
-    public MemorandumFragment(){
+    public MemorandumFragment() {
 
     }
-    private EditText et;
-    private Button btn;
-    private TextView tv;
+
+    private EditText editText;
+    private Button button;
+    private TextView textView;
     private int count;
-    private StringBuilder sb;
+    private StringBuilder stringBuilder;
     private String msg;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_memorandum,container,false);
+        return inflater.inflate(R.layout.fragment_memorandum, container, false);
     }
 
     @SuppressLint("ResourceType")
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().findViewById(R.layout.activity_main);
 //        getActivity().setContentView(R.layout.fragment_memorandum);
 
-        sb = new StringBuilder();
+        stringBuilder = new StringBuilder();
         Bundle bundle = getActivity().getIntent().getExtras();
-        if(bundle != null){
+        if (bundle != null) {
             new AlertDialog.Builder(getActivity())
                     .setIcon(null)
                     .setTitle("温馨提示")
@@ -76,43 +78,43 @@ public class MemorandumFragment extends Fragment {
     }
 
     private void InitViews() {
-        et = (EditText) getActivity().findViewById(R.id.et);
-        btn = (Button) getActivity().findViewById(R.id.alarmBtn);
-        tv = (TextView) getActivity().findViewById(R.id.memo_show);
+        editText = (EditText) getActivity().findViewById(R.id.et);
+        button = (Button) getActivity().findViewById(R.id.alarmBtn);
+        textView = (TextView) getActivity().findViewById(R.id.memo_show);
 
-        final Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(System.currentTimeMillis());//设置当前时间为c的时间
-        final int hour = c.get(Calendar.HOUR_OF_DAY);
-        final int minute = c.get(Calendar.MINUTE);
-        btn.setOnClickListener(new View.OnClickListener() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());//设置当前时间为calendar的时间
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                msg = et.getText().toString().trim();
-                sb.append(count++);
-                sb.append("备忘录的内容为:");
-                sb.append(msg);
-                sb.append("\n");
-                tv.setText(sb.toString().trim());
+                msg = editText.getText().toString().trim();
+                stringBuilder.append(count++);
+                stringBuilder.append("备忘录的内容为:");
+                stringBuilder.append(msg);
+                stringBuilder.append("\n");
+                textView.setText(stringBuilder.toString().trim());
                 new TimePickerDialog(
                         getActivity(),
-                        new TimePickerDialog.OnTimeSetListener(){
+                        new TimePickerDialog.OnTimeSetListener() {
 
                             @Override
-                            public void onTimeSet(TimePicker view,int hourOfDay, int minute) {
-                                c.setTimeInMillis(System.currentTimeMillis());
-                                c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                                c.set(Calendar.MINUTE, minute);
-                                c.set(Calendar.SECOND, 0);
-                                c.set(Calendar.MILLISECOND, 0);
-                                AlarmManager am = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
-                                Intent intent = new Intent(getActivity(),AlarmReceive.class);
-                                PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0, intent, 0);
-                                am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                calendar.setTimeInMillis(System.currentTimeMillis());
+                                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                calendar.set(Calendar.MINUTE, minute);
+                                calendar.set(Calendar.SECOND, 0);
+                                calendar.set(Calendar.MILLISECOND, 0);
+                                AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+                                Intent intent = new Intent(getActivity(), AlarmReceive.class);
+                                PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, 0);
+                                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
 
-                                String tempHour = (hour+"").length()>1?hour+"":"0"+hour;
-                                String tempMinute = (minute+"").length()>1?minute+"":"0"+minute;
-                                Toast.makeText(getActivity(), "设置时间为:"+tempHour+":"+tempMinute, Toast.LENGTH_SHORT).show();
+                                String tempHour = (hour + "").length() > 1 ? hour + "" : "0" + hour;
+                                String tempMinute = (minute + "").length() > 1 ? minute + "" : "0" + minute;
+                                Toast.makeText(getActivity(), "设置时间为:" + tempHour + ":" + tempMinute, Toast.LENGTH_SHORT).show();
                             }
                         },
                         hour,
